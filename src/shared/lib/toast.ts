51a18b228465bfb65fc43599fd4685df56649741
SUMINMOON <sum95771@gmail.com>
2024-12-01 00:53:33 +0900

    style: toast 스타일 설정

---
+++ src/shared/lib/toast.ts
@@ -0,0 +1,7 @@
+/**
+ * 토스트 상태 css
+ */
+export const TOAST = Object.freeze({
+  success: 'bg-lime-700 border-lime-700 text-white' as const,
+  error: 'bg-rose-700 border-rose-700 text-white' as const,
+});
